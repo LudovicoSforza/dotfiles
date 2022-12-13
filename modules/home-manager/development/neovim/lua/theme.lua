00359e20e40f06db1fddd91b88df9026d6800483
Ludovico Piero <ludovicopiero@pm.me>
2022-12-13 13:28:29 +1000

    a

--- modules/home-manager/development/neovim/lua/theme.lua
+++
@@ -1,7 +0,0 @@
-require("catppuccin").setup()
-
-vim.g.catppuccin_flavour = "mocha"
-vim.cmd([[colorscheme catppuccin]])
-
--- set lightline theme to horizon
-vim.g.lightline = { colorscheme = "catppuccin" }
