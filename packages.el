ce8e07dbf2a569d28c2ba74721b729500c3b0235
Ludovico Piero <ludovicopiero@pm.me>
2022-12-13 13:30:02 +1000

    b

--- packages.el
+++ packages.el
@@ -39,13 +39,6 @@
 
 ;; Use `:pin' to specify a particular commit to install.
 ;(package! builtin-package :pin "1a2b3c4d5e")
-(use-package nix-mode)
-(use-package eglot
-  :config
-  (add-to-list 'eglot-server-programs '(nix-mode . ("nil")))
-  :hook
-  (nix-mode . eglot-ensure))
-
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
 ;(unpin! pinned-package)
