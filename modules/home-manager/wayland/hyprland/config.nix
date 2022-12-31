dac187b0e3fd3f58eccb93cf15be33f5e8d8ef5f
Ludovico Piero <ludovicopiero@pm.me>
2023-01-01 08:18:33 +1000

    hyprland: aligning stuff

--- modules/home-manager/wayland/hyprland/config.nix
+++ modules/home-manager/wayland/hyprland/config.nix
@@ -14,51 +14,51 @@ in
 
     input {
         follow_mouse = 1
-        repeat_rate = 50
+        repeat_rate  = 50
         repeat_delay = 300
         touchpad {
-            natural_scroll = 1
+            natural_scroll       = 1
             disable_while_typing = 1
         }
     }
 
     general {
-        sensitivity = 1
-        gaps_in = 2
-        gaps_out = 2
-        border_size = 2
-        col.active_border = rgb(${colors.base0D}) rgb(${colors.base0E}) 270deg
+        sensitivity         = 1
+        gaps_in             = 2
+        gaps_out            = 2
+        border_size         = 2
+        col.active_border   = rgb(${colors.base0D}) rgb(${colors.base0E}) 270deg
         col.inactive_border = rgb(${colors.base00}) rgb(${colors.base01}) 270deg
-        layout = dwindle
+        layout              = dwindle
     }
 
     dwindle {
          col.group_border_active = rgb(${colors.base0B})
-         col.group_border = rgb(${colors.base00})
-         force_split = 2 # new = right or bottom
-         pseudotile = 0 # enable pseudotiling on dwindle
+         col.group_border        = rgb(${colors.base00})
+         force_split             = 2 # new = right or bottom
+         pseudotile              = 0 # enable pseudotiling on dwindle
     }
 
     decoration {
-        rounding = 0
-        blur = false
-        blur_size = 8
-        blur_passes = 2
+        rounding               = 0
+        blur                   = false
+        blur_size              = 8
+        blur_passes            = 2
         blur_new_optimizations = true
-        drop_shadow = false
-        shadow_range = 5
-        col.shadow = rgb(eba0ac)
-        shadow_render_power = 2
-        shadow_ignore_window = 1
+        drop_shadow            = false
+        shadow_range           = 5
+        col.shadow             = rgb(eba0ac)
+        shadow_render_power    = 2
+        shadow_ignore_window   = 1
     }
 
     animations {
         enabled = 1
-        bezier = customBezier , 0.79 , 0.33 , 0.14 , 0.53
-        animation = windows , 1 , 3 , customBezier
-        animation = border , 1 , 2 , default
-        animation = fade , 1 , 2 , default
-        animation = workspaces , 1 , 2 , customBezier
+        bezier    = customBezier , 0.79  , 0.33 , 0.14 , 0.53
+        animation = windows      , 1 , 3 , customBezier
+        animation = border       , 1 , 2 , default
+        animation = fade         , 1 , 2 , default
+        animation = workspaces   , 1 , 2 , customBezier
     }
 
     misc {
@@ -75,7 +75,7 @@ in
     }
 
     # window rules
-    windowrule = float , Steam
+    windowrule = float       , Steam
     windowrule = workspace 4 , Steam
     windowrule = workspace 4 , lutris
     windowrule = workspace 3 , discord
@@ -87,9 +87,10 @@ in
     windowrule = workspace 1 , jetbrains-goland
 
     # v2
-    windowrulev2 = workspace 3, title:^(WebCord)$
+    windowrulev2 = workspace 3 , title:^(WebCord)$
     windowrulev2 = workspace 5 , class:^(Spotify)$
     windowrulev2 = workspace 1 , class:^(jetbrains-clion)$
+    windowrulev2 = float, class:^(discordcanary)$ # Discord File Picker
     windowrulev2 = noblur, class:^(waybar)$
     windowrulev2 = noblur, class:^(firefox)$
     windowrulev2 = noblur, class:^(chromium-browser)$
@@ -109,34 +110,34 @@ in
     $bemenu = $HYPR_FOLDER/scripts/bemenu
 
     # Binds Keyboard
-    bind = SUPER , C , exit ,
-    bind = SUPER , D , exec , $discord
+    bind = SUPER      , C , exit ,
+    bind = SUPER      , D , exec , $discord
     bind = SUPERSHIFT , D , exec , $webcord
     bind = SUPERSHIFT , E , exec , [float] thunar
-    bind = SUPER , E , exec , emacsclient -c -a 'nvim'
-    bind = SUPER , F , fullscreen , 0
+    bind = SUPER      , E , exec , emacsclient -c -a 'nvim'
+    bind = SUPER      , F , fullscreen , 0
     bind = SUPERSHIFT , G , exec , chromium
-    bind = SUPER, G , exec , ${pkgs.firefox}/bin/firefox
-    bind = SUPER , M , exec , [workspace 5 silent;tile] mailspring
-    bind = SUPER , P , exec , $bemenu
-    bind = SUPER , Q , exec , ${pkgs.hyprpicker}/bin/hyprpicker -f hex --autocopy
-    bind = SUPER , T , togglefloating ,
-    bind = SUPER , R , togglegroup ,
-    bind = SUPERSHIFT, J , changegroupactive, f
-    bind = SUPERSHIFT, K , changegroupactive, b
-    bind = SUPER , S , exec , [workspace 5 silent;tile] spotify
-    bind = SUPER , W , killactive ,
-    bind = SUPER , X , exec , $powermenu
-    bind = CTRL , Return , exec , ${pkgs.kitty}/bin/kitty
+    bind = SUPER      , G , exec , ${pkgs.firefox}/bin/firefox
+    bind = SUPER      , M , exec , [workspace 5 silent;tile] mailspring
+    bind = SUPER      , P , exec , $bemenu
+    bind = SUPER      , Q , exec , ${pkgs.hyprpicker}/bin/hyprpicker -f hex --autocopy
+    bind = SUPER      , T , togglefloating ,
+    bind = SUPER      , R , togglegroup ,
+    bind = SUPERSHIFT , J , changegroupactive, f
+    bind = SUPERSHIFT , K , changegroupactive, b
+    bind = SUPER      , S , exec , [workspace 5 silent;tile] spotify
+    bind = SUPER      , W , killactive ,
+    bind = SUPER      , X , exec , $powermenu
+    bind = CTRL       , Return , exec , ${pkgs.kitty}/bin/kitty
     bind = SUPERSHIFT , Return , exec , foot
-    bind = SUPER  , Return , exec , ${pkgs.wezterm}/bin/wezterm
+    bind = SUPER      , Return , exec , ${pkgs.wezterm}/bin/wezterm
 
     # Binds Mouse
     bindm = SUPER , mouse:272 , movewindow
     bindm = SUPER , mouse:273 , resizewindow
 
     # Screenshot
-    bind = CTRL , Print , exec , grimblast --notify copy area
+    bind = CTRL  , Print , exec , grimblast --notify copy area
     bind = SUPER , Print , exec , grimblast --notify --cursor copysave output
 
     bind = SUPER , h , resizeactive , -20 0
@@ -173,11 +174,11 @@ in
     # bind = SUPERSHIFT , 0 , movetoworkspacesilent , 10
 
     # Media Keys
-    bind = , XF86AudioNext , exec , ${pkgs.playerctl}/bin/playerctl next
-    bind = , XF86AudioPrev , exec , ${pkgs.playerctl}/bin/playerctl previous
-    bind = , XF86AudioPlay , exec , ${pkgs.playerctl}/bin/playerctl play-pause
-    bind = , XF86AudioPause , exec , ${pkgs.playerctl}/bin/playerctl pause
-    bind = , XF86AudioStop , exec , ${pkgs.playerctl}/bin/playerctl stop
+    bind  = , XF86AudioNext , exec , ${pkgs.playerctl}/bin/playerctl next
+    bind  = , XF86AudioPrev , exec , ${pkgs.playerctl}/bin/playerctl previous
+    bind  = , XF86AudioPlay , exec , ${pkgs.playerctl}/bin/playerctl play-pause
+    bind  = , XF86AudioPause , exec , ${pkgs.playerctl}/bin/playerctl pause
+    bind  = , XF86AudioStop , exec , ${pkgs.playerctl}/bin/playerctl stop
     binde = , XF86AudioRaiseVolume , exec , ${pkgs.alsa-utils}/bin/amixer -q set Master 5%+
     binde = , XF86AudioLowerVolume , exec , ${pkgs.alsa-utils}/bin/amixer -q set Master 5%-
 
@@ -186,8 +187,6 @@ in
     binde = , XF86MonBrightnessDown , exec , ${pkgs.brightnessctl}/bin/brightnessctl set 5%-
 
     exec-once = ${pkgs.waybar}/bin/waybar
-    # exec-once = eww open bar
     exec-once = ${pkgs.hyprpaper}/bin/hyprpaper
-    # exec-once = ${pkgs.mpvpaper}/bin/mpvpaper -o "no-audio --loop-playlist" eDP-1 $HYPR_FOLDER/Wallpaper/Wallpaper.mp4
   '';
 }
