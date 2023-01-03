81cffaeeb66f77ceb3e3dcc7506a6be252e55871
Ludovico Piero <ludovicopiero@pm.me>
2023-01-04 02:47:30 +1000

    hyprland: disable NIXOS_OZONE_WL

--- modules/home-manager/wayland/hyprland/default.nix
+++ modules/home-manager/wayland/hyprland/default.nix
@@ -34,8 +34,13 @@
   ];
 
   home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
+    # NIXOS_OZONE_WL = "1";
     MOZ_ENABLE_WAYLAND = "1";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    SDL_VIDEODRIVER = "wayland";
+    XDG_SESSION_TYPE = "wayland";
   };
 
   # https://github.com/fufexan/dotfiles/blob/main/home/wayland/hyprland/default.nix#L36
